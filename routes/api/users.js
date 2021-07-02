3af72963dea4154a283b9eeb0d57b1704c6f5811
Andrew Talle <talle.andrew@gmail.com>
2021-07-02 16:03:24 -0600

    setup auth route

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator/check");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const User = require("../../models/User");
 
-// @route  GET api/users
+// @route  POST api/users
 // @desc   Register user
 // @access Public
 router.post(
@@ -52,8 +54,21 @@ router.post(
 
       await user.save();
 
-      // Return jsonwebtoken
-      res.send("User registered");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
